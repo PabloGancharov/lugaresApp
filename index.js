b94e5528200bd7f9788f8c626458f436aed9ae93
Pablo Gancharov <pablogancharov@gmail.com>
2018-04-19 22:51:20 -0300

    fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { AppRegistry } from 'react-native';
 import App from './App';
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
 
 AppRegistry.registerComponent('lugaresApp', () => App);
